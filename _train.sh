b6334557b95e94a897094261ae4969776e5b482d
NinaRag <niranjanasr383@gmail.com>
2023-01-24 14:40:21 +0000

    from colab

--- _train.sh
+++ _train.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Load python virtualenv
-virtualenv venv
-source venv/bin/activate
-pip3 install -r requirements.txt
+#virtualenv venv
+#source venv/bin/activate
+#pip3 install -r requirements.txt
 
 # Set GPU device ID
 export CUDA_VISIBLE_DEVICES=-1
@@ -17,12 +17,14 @@ export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libGLEW.so
 # Begin experiment
 python3 main.py \
 --seed 42 \
+--config "ccf_pd.yaml" \
 --opponent-shaping \
 --prefix "" \
 "$@"
 
 python3 main.py \
 --seed 42 \
+--config "ccf_pd.yaml" \
 --opponent-shaping \
 --test-mode \
 --prefix "test" \
